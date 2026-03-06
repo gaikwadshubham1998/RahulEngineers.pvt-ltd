89379a6718bfec72eacb85d0f74217c94a9be7d8
sgweb2025e <shubhamgaikwad7258@gmail.com>
2026-03-07 01:05:45 +0530

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -110,4 +110,13 @@ document.getElementById("contactForm").addEventListener("submit", function (e) {
     this.reset();
 });
 
+// smooth scrooling 
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
 
